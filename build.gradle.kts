9ae76437d6000739503845e16d5d9036d549c317
Robert Fromholz <bossymr.bossy@gmail.com>
2022-06-12 15:47:05 +0200

    Create project structure

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("org.jetbrains.intellij") version "1.6.0"
+    java
+}
+
+group = "io.github.bossymr"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("2021.3.2")
+}
+tasks {
+    patchPluginXml {
+        changeNotes.set("""""".trimIndent())
+    }
+}
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
