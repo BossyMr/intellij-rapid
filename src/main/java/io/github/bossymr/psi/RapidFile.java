dc43238604f0ebfda009718a8708452b9e79480a
Robert Fromholz <bossymr.bossy@gmail.com>
2022-06-21 20:43:09 +0200

    Create RapidParserDefinition

---
+++ src/main/java/io/github/bossymr/psi/RapidFile.java
@@ -0,0 +1,20 @@
+package io.github.bossymr.psi;
+
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import io.github.bossymr.language.RapidFileType;
+import io.github.bossymr.language.RapidLanguage;
+import org.jetbrains.annotations.NotNull;
+
+public class RapidFile extends PsiFileBase {
+
+    public RapidFile(@NotNull FileViewProvider viewProvider) {
+        super(viewProvider, RapidLanguage.INSTANCE);
+    }
+
+    @Override
+    public @NotNull FileType getFileType() {
+        return RapidFileType.INSTANCE;
+    }
+}
