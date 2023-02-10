fa57863600765da07fad91812ca5f8efc42da17c
Robert Fromholz <bossymr.bossy@gmail.com>
2023-02-10 13:02:14 +0100

    Create ConfigurationType

---
+++ src/main/java/com/bossymr/rapid/ide/execution/configurations/RapidLocalConfigurationFactory.java
@@ -0,0 +1,40 @@
+package com.bossymr.rapid.ide.execution.configurations;
+
+import com.bossymr.rapid.RapidBundle;
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A {@code ConfigurationFactory} used to create run configurations to execute a local module.
+ */
+public class RapidLocalConfigurationFactory extends ConfigurationFactory {
+
+    public RapidLocalConfigurationFactory(@NotNull ConfigurationType type) {
+        super(type);
+    }
+
+    @Override
+    public @NotNull @NonNls String getId() {
+        return "RapidLocalConfigurationType";
+    }
+
+    @Override
+    public @NotNull @Nls String getName() {
+        return RapidBundle.message("run.configuration.factory.local");
+    }
+
+    @Override
+    public boolean isEditableInDumbMode() {
+        return true;
+    }
+
+    @Override
+    public @NotNull RunConfiguration createTemplateConfiguration(@NotNull Project project) {
+        return null;
+    }
+}
