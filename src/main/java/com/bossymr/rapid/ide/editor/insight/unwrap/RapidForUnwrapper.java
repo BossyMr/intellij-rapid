a76ebd1aefa2161efe1a3e6fb4f14b53b7cef848
Robert Fromholz <bossymr.bossy@gmail.com>
2024-04-05 11:25:35 +0200

    Add more statement unwrappers

--- src/main/java/com/bossymr/rapid/ide/editor/insight/unwrap/RapidForUnwrapper.java
+++ src/main/java/com/bossymr/rapid/ide/editor/insight/unwrap/RapidForUnwrapper.java
@@ -1,33 +1,28 @@
 package com.bossymr.rapid.ide.editor.insight.unwrap;
 
 import com.bossymr.rapid.RapidBundle;
+import com.bossymr.rapid.language.psi.RapidForStatement;
 import com.bossymr.rapid.language.psi.RapidIfStatement;
 import com.bossymr.rapid.language.psi.RapidStatementList;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 
-public class RapidIfUnwrapper extends RapidUnwrapper {
+public class RapidForUnwrapper extends RapidUnwrapper {
 
-    public RapidIfUnwrapper() {
-        super(RapidBundle.message("unwrap.if"));
+    public RapidForUnwrapper() {
+        super(RapidBundle.message("unwrap.for"));
     }
 
     @Override
     public boolean isApplicableTo(@NotNull PsiElement e) {
-        if (!(e instanceof RapidIfStatement)) {
-            return false;
-        }
-        // If the parent is also an if-statement, the current if-statement is an else block.
-        // This if-statement can't be an in the then block, because then it's parent would be a statement list.
-        PsiElement parent = e.getParent();
-        return !(parent instanceof RapidIfStatement);
+        return e instanceof RapidForStatement;
     }
 
     @Override
     protected void doUnwrap(@NotNull PsiElement element, @NotNull Context context) throws IncorrectOperationException {
-        RapidIfStatement statement = (RapidIfStatement) element;
-        RapidStatementList thenBranch = statement.getThenBranch();
+        RapidForStatement statement = ((RapidForStatement) element);
+        RapidStatementList thenBranch = statement.getStatementList();
         context.extractStatementList(thenBranch, element);
         context.delete(element);
     }
